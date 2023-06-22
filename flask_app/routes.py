09c116f7a6b597bea3e1d523330d7b630b653fdc
7TTY <chris.harbster@gmail.com>
2023-06-21 21:46:08 -0700

    test post request for access token

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -108,8 +108,8 @@ def spotify_callback():
 
     base64encoded = base64.b64encode(("{}:{}".format(CLIENT_ID, CLIENT_SECRET)).encode())
     headers = {
-#        'Authorization': "Basic {}".format(base64encoded.decode()),
-        'Authorization': "Basic {}".format(base64encoded),
+        'Authorization': "Basic {}".format(base64encoded.decode()),
+
         'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded'
     }
