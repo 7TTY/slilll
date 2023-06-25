058648431d9e10c4bda1839b2c08fe21ed927b2e
7TTY <chris.harbster@gmail.com>
2023-06-24 21:20:23 -0700

    add config.json, setup app.config for client creds

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+
+
+with open('/home/utterpop/slilll/flask_app/config.json') as config_file:
+	config = json.load(config_file)
+
+app.config['CLIENT_ID'] = config.get('CLIENT_ID')
+app.config['CLIENT_SECRET'] = config.get('CLIENT_SECRET')
 
 from flask_app import routes
